d5701b5112a5af51829d4c43138917d7777ada03
oNe1Rot <72159693+oNe1Rot@users.noreply.github.com>
2020-10-09 20:32:43 +0530

    Ask user age and Help instruction

--- Car Game.py
+++ Car Game.py
@@ -1,6 +1,12 @@
-command = ""
-started = False
-while True:
+
+user_age=int(input("Please ENTER your age"))
+
+if user_age>8:
+  print("You are eligible to play this game!")
+  print("Enter HELP,for instructions: ")
+  command = ""
+  started = False
+  while True:
     command = input("> ").lower()
     if command == "help":
         print("""
@@ -8,6 +14,8 @@ start - to start a car
 stop - to stop a car
 quit - to 
          """)
+        
+        
     elif command == "start":
         if started:
             print("Car is already started")
@@ -20,8 +28,12 @@ quit - to
         else:
             started = False
             print("Car stopped")  
-        
+
     elif command == "quit":
         break
     else:
-        print("I don't understand...")                 
+        print("I don't understand...")
+        
+elif user_age<=8:
+   print("You are not eligible to play this game (")
+
